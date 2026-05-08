6147bab92f4498502973be7ff653e10e86a3cab8
Benjamin Pinto <pintoperaltabenjaminandres@gmail.com>
2026-05-08 11:13:32 -0400

    primer commit

---
+++ ejercicios.js
@@ -0,0 +1,52 @@
+let nombre = "Benjamin";
+let apellido = "Pinto";
+
+
+console.log (nombre + " " + apellido);
+let comida_Favorita = "lasaña";
+console.log (comida_Favorita);
+
+let a = 10// "" transforma un numero en string 
+let b = 3
+
+/*si tratas de sumar 2 strings solo se van a juntar como texto sin hacer el calculo
+"===" compara tanto el valor como el tipo de dato
+&& ambas condiciones son verdaderas */
+
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a % b);
+
+let edad = 15;
+console.log (edad > 18);
+console.log (edad < 18);
+
+let numero = 8;
+
+console.log (numero > 5 && numero < 10);
+
+let nota = 6
+
+console.log ( nota == 7);
+
+let temperatura = 25;
+
+console.log (temperatura >= 30);
+
+let numero1 = 35
+let numero2 = 21
+
+console.log (numero1 == numero2);
+console.log (numero1 > numero2);
+
+let edad2 = 17
+let tienepermiso = true
+
+console.log (edad2 >= 18 || tienepermiso == true);
+
+let usuario = "admin"
+let password = "1234"
+
+console.log (usuario == "adming" && password == "1234")
+
